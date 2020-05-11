b50f546c5ce5cd54eaede90e01352567e8217609
dengdengdeng <dengdengdeng@192.168.0.103>
2020-05-11 21:12:18 +0800

    feat: 创建项目

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/ani-canvas-point',
+    name: 'ani1',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ani-canvas-point/index.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/ani-canvas-point/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        我是页面{{num}}
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            num:1
+        }
+    }
+}
+</script>
